7f9c6612f875cef02cbb1f9efb0ce951badd43d0
orangain <orangain@gmail.com>
2017-12-06 00:04:30 +0900

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scrapy-s3pipeline',
-      version='0.1.1',
+      version='0.2.0',
       description='Scrapy pipeline to store chunked items into AWS S3 bucket',
       classifiers=[
         'Development Status :: 4 - Beta',
